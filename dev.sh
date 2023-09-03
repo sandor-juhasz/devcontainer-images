fdfbd440c06e63d9e204d1327fefa727ac4914db
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2023-09-03 06:24:04 +0200

    Adding dotfiles utilities preinstaller to the images

---
+++ dev.sh
@@ -0,0 +1,15 @@
+function build_and_publish_image() {
+    local image_dir="$1"
+
+    if [[ ! -d "${image_dir}" ]]; then
+        echo "ERROR: cannot find image directory: ${image_dir}"
+        return 1
+    fi
+
+    local image="ghcr.io/sandor-juhasz/devcontainer-images/${image_dir}:latest"
+
+    devcontainer build --workspace-folder "${image_dir}" \
+        --image-name "${image}" \
+        --buildkit never
+    docker push "${image}"
+}
